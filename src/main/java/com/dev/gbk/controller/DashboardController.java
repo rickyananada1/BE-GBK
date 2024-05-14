0a2378ab77bd85793f69d66b174e33b7fbc5df93
andy.pratama <96979295+rickyananada1@users.noreply.github.com>
2024-05-14 11:08:19 +0700

    Push DashboardController

---
+++ src/main/java/com/dev/gbk/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package com.dev.gbk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DashboardController {
+        @GetMapping("/dashboard")
+        public String dashboard() {
+            return "Welcome to the dashboard!";
+        }
+}
