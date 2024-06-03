6c281fc47705415795abba7b497cd9ce8489f753
jhonreimons <jhonreimons95@gmail.com>
2024-06-04 00:25:01 +0700

    update swagger

--- src/main/java/com/dev/gbk/config/WebMvcConfig.java
+++ src/main/java/com/dev/gbk/config/WebMvcConfig.java
@@ -1,18 +1,16 @@
 package com.dev.gbk.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v3/api-docs/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
