7a029909160246344a637972a4b582932009fb8a
jhonreimons <jhonreimons95@gmail.com>
2024-06-04 00:20:49 +0700

    update swagger

--- src/main/java/com/dev/gbk/config/WebMvcConfig.java
+++ src/main/java/com/dev/gbk/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v3/api-docs/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", POST, PUT, DELETE, OPTIONS")
+                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
