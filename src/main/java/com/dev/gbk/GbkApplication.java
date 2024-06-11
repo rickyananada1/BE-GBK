155cd8dee07663055cab37b5ecc03854728580ac
andy.pratama <96979295+rickyananada1@users.noreply.github.com>
2024-06-11 21:01:52 +0700

    Update CORS

--- src/main/java/com/dev/gbk/GbkApplication.java
+++ src/main/java/com/dev/gbk/GbkApplication.java
@@ -2,11 +2,24 @@ package com.dev.gbk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GbkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GbkApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
