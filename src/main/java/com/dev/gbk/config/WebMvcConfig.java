9c6f3b682f868b5de6fb63e5d1dea1d4688e3b79
andy.pratama <96979295+rickyananada1@users.noreply.github.com>
2024-06-11 22:21:44 +0700

    Update CORS

--- src/main/java/com/dev/gbk/config/WebMvcConfig.java
+++ src/main/java/com/dev/gbk/config/WebMvcConfig.java
@@ -12,11 +12,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v3/api-docs/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173/","http://localhost:8000/","http://213.210.37.2:8081/")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173/","http://localhost:8000/","http://213.210.37.2:8081/")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
