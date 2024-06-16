0dd39bec4e0d0579443e936bd27a36746774f6b4
marloxxx <66661134+marloxxx@users.noreply.github.com>
2024-06-16 14:57:07 +0700

    update

--- src/main/java/com/dev/gbk/config/SecurityConfig.java
+++ src/main/java/com/dev/gbk/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api-docs/**").permitAll()
                         .anyRequest().authenticated()
 
                 ).exceptionHandling(exception -> exception
