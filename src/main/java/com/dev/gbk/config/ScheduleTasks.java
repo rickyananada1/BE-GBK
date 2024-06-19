7a322a29023a6b5f761f6177644c5eaef2e97eb4
ricky.sitorus <96979295+rickyananada1@users.noreply.github.com>
2024-06-19 23:27:36 +0700

    Scheduler Config

---
+++ src/main/java/com/dev/gbk/config/ScheduleTasks.java
@@ -0,0 +1,45 @@
+package com.dev.gbk.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ScheduleTasks {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleTasks.class);
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Scheduled(fixedRate = 2000)
+    public void scheduleTaskWithFixedRate() {
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            logger.error("Ran into an error {}", ex);
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+    public void scheduleTaskWithInitialDelay() {
+        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+    }
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduleTaskWithCronExpression() {
+        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+    }
+
+}
